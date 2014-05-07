3998daf617dd7f6aa46797ff218584711c1b9f16
Chris Allulis <Callulis@btfiloh.org>
2014-05-07 15:36:44 -0400

    omgzCommit

---
+++ Arithmetic.py
@@ -0,0 +1,17 @@
+# Chris Allulis
+# What does this program do?
+
+def main():
+    num1 = 10
+    num2 = 3
+    print num1 +  num2
+    print num1 -  num2
+    print num1 *  num2
+    print num1 /  num2
+    print num1 // num2
+    print num1 %  num2
+    print num1 ** num2
+
+if __name__ == "__main__":
+    main()
+
