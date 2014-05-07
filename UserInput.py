7c06441309b47c2ff0d468cfbe7dea605236a48c
Callulis <Callulis@btfiloh.org>
2014-05-07 17:51:53 -0400

    Testing

--- UserInput.py
+++ UserInput.py
@@ -4,7 +4,8 @@
 def main():
     name = raw_input("What is your name?")
     print "Hello, " + name + "!"
-
+    age = int(raw_input("How old are you?"))
+    print "You are " + str(age) + " years old!"
 if __name__ == "__main__":
     main()
 
